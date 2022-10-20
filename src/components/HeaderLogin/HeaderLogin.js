e9c94e24237c47c2e535ec7402f0c73dac052b2f
stEugene <st_eugene@mail.ru>
2022-10-20 16:52:44 +0300

    burger menu update

--- src/components/HeaderLogin/HeaderLogin.js
+++ src/components/HeaderLogin/HeaderLogin.js
@@ -8,8 +8,12 @@ function Header() {
     <header className="headerLogin">
       <div className="headerLogin__box">
         <HeaderLink/>
+        <div className={navBurger ? 'headerLogin__cover_active' : 'headerLogin__cover'}></div>
         <nav className={`headerLogin__nav ${navBurger ? 'headerLogin__nav_burger' : ''}`}>
           <ul className="headerLogin__nav-list">
+            <li className={navBurger ? 'headerLogin__nav-item_burger' : 'headerLogin__nav-item headerLogin__nav-item_hide'}>
+              <a className={navBurger ? 'headerLogin__nav-text' : ''} href="/main">Главная</a>
+            </li>
             <li className={navBurger ? 'headerLogin__nav-item_burger' : 'headerLogin__nav-item headerLogin__nav-item_movie'}>
               <a className={navBurger ? 'headerLogin__nav-text' : 'headerLogin__nav-text__movies'} href="/movies">Фильмы</a>
             </li>
@@ -17,18 +21,17 @@ function Header() {
               <a className={navBurger ? 'headerLogin__nav-text' : 'headerLogin__nav-text__saved-movies'} href="/saved-movies">Сохраненные фильмы</a>
             </li>
             <li className={navBurger ? 'headerLogin__nav-item_burger' : 'headerLogin__nav-item_btn'}>
-              <a className={navBurger ? 'headerLogin__nav-text' : 'headerLogin__nav-text_btn'} href="/profile">Аккаунт</a>
+              <a className={navBurger ? 'headerLogin__nav-text headerLogin__nav-text_burgerBtn' : 'headerLogin__nav-text_btn'} href="/profile">Аккаунт</a>
             </li>
           </ul>
         </nav>
+        <div
+          className={`headerLogin__nav-burger-btn ${navBurger ? 'headerLogin__nav-burger-btn_active' : ''}`}
+          onClick={() => setNavBurger(!navBurger)}
+        >
+          <span className={`headerLogin__nav-burger-line ${navBurger ? 'headerLogin__nav-burger-line_burger' : ''}`}></span>
+        </div>
       </div>
-      <div
-        className={`headerLogin__nav-burger-btn ${navBurger ? 'headerLogin__nav-burger-btn_active' : ''}`}
-        onClick={() => setNavBurger(!navBurger)}
-      >
-        <span className={`headerLogin__nav-burger-line ${navBurger ? 'headerLogin__nav-burger-line_burger' : ''}`}></span>
-      </div>
-
     </header>
   )
 }
