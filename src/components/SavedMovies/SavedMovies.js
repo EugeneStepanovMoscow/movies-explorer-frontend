b9aaba8125918ad27f3974d2033baf5b778d9ae5
stEugene <st_eugene@mail.ru>
2022-10-22 18:24:42 +0300

    saved movies update

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -1,18 +1,23 @@
 import HeaderLogin from '../HeaderLogin/HeaderLogin'
 import Footer from '../Footer/Footer'
 import SearchForm from '../SearchForm/SearchForm'
-// import MoviesCardList from '../MoviesCardList/MoviesCardList'
+import MoviesCardList from '../MovisCardList/MoviesCardList'
 
 function SavedMovies ()
 {
 return (
   <>
-    <HeaderLogin/>
-    <main>
-      <SearchForm/>
-      {/* <MoviesCardList/> */}
-    </main>
-    <Footer/>
+      <HeaderLogin/>
+      <main className='movies'>
+        <SearchForm/>
+        <section className='movies__section'>
+          <MoviesCardList/>
+        </section>
+        <section className='movies__more'>
+          <button className='movies__more-button'>Ещё</button>
+        </section>
+      </main>
+      <Footer/>
   </>
 )
 }
